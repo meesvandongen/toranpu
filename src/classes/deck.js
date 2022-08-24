0462050f4af2b2c37e12bbbc9f520811aee3997f
MaestroDagan <ditisdagan@hotmail.com>
2022-08-24 21:53:50 +0200

    v0.1.1

--- src/classes/deck.js
+++ src/classes/deck.js
@@ -1,5 +1,9 @@
 const generateUUID = require("generate-uuid.js");
 
+/**
+ * Creates a standard deck of playing cards (without jokers)
+ * @returns {Array} All the cards in a standard deck of playing cards (without jokers)
+ */
 function createDeck() {
   const NAMES = [
     "1",
@@ -23,12 +27,19 @@ function createDeck() {
 }
 
 module.exports = class Deck {
+  /**
+   * Gets called when the Deck class is instantiated
+   */
   constructor() {
     this.cards = createDeck();
 
     this.id = generateUUID();
   }
 
+  /**
+   * Shuffles the deck
+   * @returns {Deck} A shuffled deck of playing cards
+   */
   shuffle() {
     for (let i = this.deck.length - 1; i > 0; i--) {
       const j = Math.floor(Math.random() * (i + 1));
@@ -38,10 +49,18 @@ module.exports = class Deck {
     return this;
   }
 
+  /**
+   * Draws a card
+   * @returns {string} The card that's drawn
+   */
   draw() {
     return this.cards.shift();
   }
 
+  /**
+   * Resets the deck to its initial state
+   * @returns {Deck} A standard deck of playing cards (without jokers)
+   */
   reset() {
     this.cards = createDeck();
 
