ce84e92f581348ed3f2b5cfbcbad3610a3ffc8d2
MaestroDagan <ditisdagan@hotmail.com>
2022-08-25 19:27:49 +0200

    Patience: Added move()
    With this method you can move a card from anywhere to anywhere (you can't move anything to or from the stock and you can't move anything to the talon)

--- src/classes/patience.js
+++ src/classes/patience.js
@@ -64,4 +64,46 @@ module.exports = class Patience {
 
     return card;
   }
+
+  move(cardLocation, card, destination) {
+    const cardLocationArray = cardLocation.split("[");
+    const destinationArray = destination.split("[");
+
+    if (cardLocationArray[1]) {
+      if (this[cardLocationArray[0]][parseInt(cardLocationArray[1])].open) {
+        this[cardLocationArray[0]][parseInt(cardLocationArray[1])].open.splice(
+          this[cardLocationArray[0]][
+            parseInt(cardLocationArray[1])
+          ].open.indexOf(card)
+        );
+
+        if (
+          !this[cardLocationArray[0]][parseInt(cardLocationArray[1])].open[0] &&
+          this[cardLocationArray[0]][parseInt(cardLocationArray[1])].closed[0]
+        ) {
+          this[cardLocationArray[0]][parseInt(cardLocationArray[1])].open.push(
+            this[cardLocationArray[0]][
+              parseInt(cardLocationArray[1])
+            ].closed.shift()
+          );
+        }
+      } else {
+        this[cardLocationArray[0]][parseInt(cardLocationArray[1])].splice(
+          this[cardLocationArray[0]][parseInt(cardLocationArray[1])].indexOf(
+            card
+          )
+        );
+      }
+    } else {
+      this.talon.splice(this.talon.indexOf(card));
+    }
+
+    if (this[destinationArray[0]][parseInt(destinationArray[1])].open) {
+      this[destinationArray[0]][parseInt(destinationArray[1])].open.push(card);
+    } else {
+      this[destinationArray[0]][parseInt(destinationArray[1])].push(card);
+    }
+
+    return this;
+  }
 };
