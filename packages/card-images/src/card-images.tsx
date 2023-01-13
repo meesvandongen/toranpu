cc51fae5dd1484834dec3426ba65c5f393b4073a
Mees van Dongen <meesvandongen@gmail.com>
2023-01-13 17:59:06 +0100

    card images folder

---
+++ packages/card-images/src/card-images.tsx
@@ -0,0 +1,25 @@
+import satori from "satori";
+import sharp from "sharp";
+import fs from "fs/promises";
+
+const svg = await satori(<div style={{ color: "black" }}>hello, world</div>, {
+  width: 600,
+  height: 400,
+  fonts: [
+    {
+      name: "Roboto",
+      // Use `fs` (Node.js only) or `fetch` to read the font as Buffer/ArrayBuffer and provide `data` here.
+      data: await fs.readFile(
+        "./node_modules/@fontsource/roboto/files/roboto-all-500-normal.woff"
+      ),
+      weight: 400,
+      style: "normal",
+    },
+  ],
+});
+
+const svgBuffer = Buffer.from(svg);
+
+const png = await sharp(svgBuffer, { density: 300 }).png().toBuffer();
+
+await fs.writeFile("card.png", png);
