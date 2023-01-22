5b644856b9af4b291f5964b3ee968680ad0e88cd
Mees van Dongen <meesvandongen@gmail.com>
2023-01-22 15:58:56 +0100

    temp remove toranpu-gui

--- apps/toranpu-gui/src/dnd/dnd-types.ts
+++
@@ -1,8 +0,0 @@
-import { MutableRefObject } from "react";
-
-export type UniqueIdentifier = string | number;
-type AnyData = Record<string, any>;
-export type Data<T = AnyData> = T & AnyData;
-
-export type DataRef<T = AnyData> = MutableRefObject<Data<T> | undefined>;
-
